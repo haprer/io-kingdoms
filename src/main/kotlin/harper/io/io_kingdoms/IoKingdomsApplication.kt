834f38e4bc560334449b19c3c0c847d7690c57fc
haprer <harpersullivan00@gmail.com>
2024-05-26 17:33:01 -0400

    Initial commit

---
+++ src/main/kotlin/harper/io/io_kingdoms/IoKingdomsApplication.kt
@@ -0,0 +1,11 @@
+package harper.io.io_kingdoms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IoKingdomsApplication
+
+fun main(args: Array<String>) {
+	runApplication<IoKingdomsApplication>(*args)
+}
